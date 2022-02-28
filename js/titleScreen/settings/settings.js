db80b1173dc30457ad8e74d8263b669df91bfa39
TeamSEA <100610345+TeamSEA@users.noreply.github.com>
2022-02-28 10:53:29 -0800

    Create settings.js

---
+++ js/titleScreen/settings/settings.js
@@ -0,0 +1,11 @@
+//settings Open Functions
+function settingsOpen() {
+  document.getElementById('settings').style.display = "block"
+  trans4.style.display = "none"
+}
+
+//setting Close Function
+function settingsClose() {
+  settings.style.display = "none"
+  trans4.style.display = "block"
+}
