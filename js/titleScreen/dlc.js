a124814a3f46ae61a8770f4d1fd19708bedf04f9
TeamSEA <100610345+TeamSEA@users.noreply.github.com>
2022-02-28 10:06:52 -0800

    Create dlc.js

---
+++ js/titleScreen/dlc.js
@@ -0,0 +1,23 @@
+//DLC
+function AC12() {
+  dlc.style.display = "block";
+  trans4.style.display = "none";
+}
+
+function backAC() {
+  dlc.style.display = "none";
+  trans4.style.display = "block";
+  dlc1_1.style.display = "none";
+}
+
+function DLC2() {
+  dlc.style.display = "none"
+  dlc1_1.style.display = "block"
+}
+
+function DLC1() {
+  dlc1_1.style.display = "none"
+  dlc.style.display = "block"
+}
+
+//
