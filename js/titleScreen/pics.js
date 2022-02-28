2afa4edd73d9cf182be6932a489c3dad11422eb9
TeamSEA <100610345+TeamSEA@users.noreply.github.com>
2022-02-28 10:50:10 -0800

    Create pics.js

---
+++ js/titleScreen/pics.js
@@ -0,0 +1,13 @@
+//title screen pics
+function picsStart() {
+  pictures.style.display = "block"
+  trans4.style.display = "none"
+  document.body.style.overflowY = "visible"
+  document.body.style.overflowX = "hidden"
+}
+
+function backPics() {
+  pictures.style.display = "none";
+  trans4.style.display = "block";
+  document.body.style.overflowY = "hidden";
+}
